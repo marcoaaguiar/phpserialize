adb8678155998013e3af97b851774bb72c6d3b38
Marco Aguiar <marco.aguiar@faci.ly>
2021-09-24 19:54:00 -0300

    Feat: add parsing for references

--- phpserialize.py
+++ phpserialize.py
@@ -236,6 +236,9 @@ r"""
     Changelog
     =========
 
+    1.4
+        -   added suport for reference ('r:')
+
     1.3
         -   added support for Python 3
 
@@ -253,11 +256,13 @@ r"""
     license: BSD
 """
 import codecs
+from collections import namedtuple
+
 try:
-    codecs.lookup_error('surrogateescape')
-    default_errors = 'surrogateescape'
+    codecs.lookup_error("surrogateescape")
+    default_errors = "surrogateescape"
 except LookupError:
-    default_errors = 'strict'
+    default_errors = "strict"
 
 try:
     from StringIO import StringIO as BytesIO
@@ -282,27 +287,40 @@ try:
 except NameError:
     xrange = range
 
-__author__ = 'Armin Ronacher <armin.ronacher@active-4.com>'
-__version__ = '1.3'
-__all__ = ('phpobject', 'convert_member_dict', 'dict_to_list', 'dict_to_tuple',
-           'load', 'loads', 'dump', 'dumps', 'serialize', 'unserialize')
+__author__ = "Armin Ronacher <armin.ronacher@active-4.com>"
+__version__ = "1.3"
+__all__ = (
+    "phpobject",
+    "convert_member_dict",
+    "dict_to_list",
+    "dict_to_tuple",
+    "load",
+    "loads",
+    "dump",
+    "dumps",
+    "serialize",
+    "unserialize",
+)
+
+Reference = namedtuple("Reference", ["position"])
 
 
 def _translate_member_name(name):
-    if name[:1] == ' ':
+    if name[:1] == " ":
         name = name.split(None, 2)[-1]
     return name
 
 
 class phpobject(object):
-    """Simple representation for PHP objects.  This is used """
-    __slots__ = ('__name__', '__php_vars__')
+    """Simple representation for PHP objects.  This is used"""
+
+    __slots__ = ("__name__", "__php_vars__")
 
     def __init__(self, name, d=None):
         if d is None:
             d = {}
-        object.__setattr__(self, '__name__', name)
-        object.__setattr__(self, '__php_vars__', d)
+        object.__setattr__(self, "__name__", name)
+        object.__setattr__(self, "__php_vars__", d)
 
     def _asdict(self):
         """Returns a new dictionary from the data with Python identifiers."""
@@ -326,7 +344,7 @@ class phpobject(object):
         self.__php_vars__[name] = value
 
     def __repr__(self):
-        return '<phpobject %r>' % (self.__name__,)
+        return "<phpobject %r>" % (self.__name__,)
 
 
 def convert_member_dict(d):
@@ -342,45 +360,46 @@ def convert_member_dict(d):
     return dict((_translate_member_name(k), v) for k, v in d.items())
 
 
-def dumps(data, charset='utf-8', errors=default_errors, object_hook=None):
+def dumps(data, charset="utf-8", errors=default_errors, object_hook=None):
     """Return the PHP-serialized representation of the object as a string,
     instead of writing it to a file like `dump` does.  On Python 3
     this returns bytes objects, on Python 3 this returns bytestrings.
     """
+
     def _serialize(obj, keypos):
         if keypos:
             if isinstance(obj, (int, long, float, bool)):
-                return ('i:%i;' % obj).encode('latin1')
+                return ("i:%i;" % obj).encode("latin1")
             if isinstance(obj, basestring):
                 encoded_obj = obj
                 if isinstance(obj, unicode):
                     encoded_obj = obj.encode(charset, errors)
                 s = BytesIO()
-                s.write(b's:')
-                s.write(str(len(encoded_obj)).encode('latin1'))
+                s.write(b"s:")
+                s.write(str(len(encoded_obj)).encode("latin1"))
                 s.write(b':"')
                 s.write(encoded_obj)
                 s.write(b'";')
                 return s.getvalue()
             if obj is None:
                 return b's:0:"";'
-            raise TypeError('can\'t serialize %r as key' % type(obj))
+            raise TypeError("can't serialize %r as key" % type(obj))
         else:
             if obj is None:
-                return b'N;'
+                return b"N;"
             if isinstance(obj, bool):
-                return ('b:%i;' % obj).encode('latin1')
+                return ("b:%i;" % obj).encode("latin1")
             if isinstance(obj, (int, long)):
-                return ('i:%s;' % obj).encode('latin1')
+                return ("i:%s;" % obj).encode("latin1")
             if isinstance(obj, float):
-                return ('d:%s;' % obj).encode('latin1')
+                return ("d:%s;" % obj).encode("latin1")
             if isinstance(obj, basestring):
                 encoded_obj = obj
                 if isinstance(obj, unicode):
                     encoded_obj = obj.encode(charset, errors)
                 s = BytesIO()
-                s.write(b's:')
-                s.write(str(len(encoded_obj)).encode('latin1'))
+                s.write(b"s:")
+                s.write(str(len(encoded_obj)).encode("latin1"))
                 s.write(b':"')
                 s.write(encoded_obj)
                 s.write(b'";')
@@ -394,25 +413,30 @@ def dumps(data, charset='utf-8', errors=default_errors, object_hook=None):
                 for key, value in iterable:
                     out.append(_serialize(key, True))
                     out.append(_serialize(value, False))
-                return b''.join([
-                    b'a:',
-                    str(len(obj)).encode('latin1'),
-                    b':{',
-                    b''.join(out),
-                    b'}'
-                ])
+                return b"".join(
+                    [b"a:", str(len(obj)).encode("latin1"), b":{", b"".join(out), b"}"]
+                )
             if isinstance(obj, phpobject):
-                return b'O' + _serialize(obj.__name__, True)[1:-1] + \
-                       _serialize(obj.__php_vars__, False)[1:]
+                return (
+                    b"O"
+                    + _serialize(obj.__name__, True)[1:-1]
+                    + _serialize(obj.__php_vars__, False)[1:]
+                )
             if object_hook is not None:
                 return _serialize(object_hook(obj), False)
-            raise TypeError('can\'t serialize %r' % type(obj))
+            raise TypeError("can't serialize %r" % type(obj))
 
     return _serialize(data, False)
 
 
-def load(fp, charset='utf-8', errors=default_errors, decode_strings=False,
-         object_hook=None, array_hook=None):
+def load(
+    fp,
+    charset="utf-8",
+    errors=default_errors,
+    decode_strings=False,
+    object_hook=None,
+    array_hook=None,
+):
     """Read a string from the open file object `fp` and interpret it as a
     data stream of PHP-serialized objects, reconstructing and returning
     the original object hierarchy.
@@ -430,18 +454,19 @@ def load(fp, charset='utf-8', errors=default_errors, decode_strings=False,
     class data members.  The data member names are in PHP format which is
     usually not what you want.  The `simple_object_hook` function can convert
     them to Python identifier names.
-
     If an `array_hook` is given that function is called with a list of pairs
     for all array items.  This can for example be set to
     `collections.OrderedDict` for an ordered, hashed dictionary.
     """
+    stack = []
+
     if array_hook is None:
         array_hook = dict
 
     def _expect(e):
         v = fp.read(len(e))
         if v != e:
-            raise ValueError('failed expectation, expected %r got %r' % (e, v))
+            raise ValueError("failed expectation, expected %r got %r" % (e, v))
 
     def _read_until(delim):
         buf = []
@@ -450,79 +475,92 @@ def load(fp, charset='utf-8', errors=default_errors, decode_strings=False,
             if char == delim:
                 break
             elif not char:
-                raise ValueError('unexpected end of stream')
+                raise ValueError("unexpected end of stream")
             buf.append(char)
-        return b''.join(buf)
+        return b"".join(buf)
 
     def _load_array():
-        items = int(_read_until(b':')) * 2
-        _expect(b'{')
+        n_items = int(_read_until(b":")) * 2
+        _expect(b"{")
         result = []
+        stack.append(result)
         last_item = Ellipsis
-        for idx in xrange(items):
+        for _ in range(n_items):
             item = _unserialize()
             if last_item is Ellipsis:
                 last_item = item
             else:
+                if isinstance(item, Reference):
+                    item = stack[item.position - 1][-1]
                 result.append((last_item, item))
+                stack.append(result[-1])
                 last_item = Ellipsis
-        _expect(b'}')
+        _expect(b"}")
         return result
 
     def _unserialize():
         type_ = fp.read(1).lower()
-        if type_ == b'n':
-            _expect(b';')
+        if type_ == b"n":
+            _expect(b";")
             return None
-        if type_ in b'idb':
-            _expect(b':')
-            data = _read_until(b';')
-            if type_ == b'i':
+        if type_ in b"idb":
+            _expect(b":")
+            data = _read_until(b";")
+            if type_ == b"i":
                 return int(data)
-            if type_ == b'd':
+            if type_ == b"d":
                 return float(data)
             return int(data) != 0
-        if type_ == b's':
-            _expect(b':')
-            length = int(_read_until(b':'))
+        if type_ == b"s":
+            _expect(b":")
+            length = int(_read_until(b":"))
             _expect(b'"')
             data = fp.read(length)
             _expect(b'"')
             if decode_strings:
                 data = data.decode(charset, errors)
-            _expect(b';')
+            _expect(b";")
             return data
-        if type_ == b'a':
-            _expect(b':')
+        if type_ == b"a":
+            _expect(b":")
             return array_hook(_load_array())
-        if type_ == b'o':
+        if type_ == b"o":
             if object_hook is None:
-                raise ValueError('object in serialization dump but '
-                                 'object_hook not given.')
-            _expect(b':')
-            name_length = int(_read_until(b':'))
+                raise ValueError(
+                    "object in serialization dump but " "object_hook not given."
+                )
+            _expect(b":")
+            name_length = int(_read_until(b":"))
             _expect(b'"')
             name = fp.read(name_length)
             _expect(b'":')
             if decode_strings:
                 name = name.decode(charset, errors)
             return object_hook(name, dict(_load_array()))
-        raise ValueError('unexpected opcode')
+        if type_ == b"r":
+            _expect(b":")
+            return Reference(int(_read_until(b";")))
+        raise ValueError("unexpected opcode")
 
     return _unserialize()
 
 
-def loads(data, charset='utf-8', errors=default_errors, decode_strings=False,
-          object_hook=None, array_hook=None):
+def loads(
+    data,
+    charset="utf-8",
+    errors=default_errors,
+    decode_strings=False,
+    object_hook=None,
+    array_hook=None,
+):
     """Read a PHP-serialized object hierarchy from a string.  Characters in the
     string past the object's representation are ignored.  On Python 3 the
     string must be a bytestring.
     """
-    return load(BytesIO(data), charset, errors, decode_strings,
-                object_hook, array_hook)
+    return load(BytesIO(data), charset, errors, decode_strings, object_hook, array_hook)
 
 
-def dump(data, fp, charset='utf-8', errors=default_errors, object_hook=None):
+def dump(data, fp, charset="utf-8", errors=default_errors, object_hook=None):
     """Write a PHP-serialized representation of obj to the open file object
     `fp`.  Unicode strings are encoded to `charset` with the error handling
     of `errors`.
@@ -547,7 +585,7 @@ def dict_to_list(d):
     try:
         return [d[x] for x in xrange(len(d))]
     except KeyError:
-        raise ValueError('dict is not a sequence')
+        raise ValueError("dict is not a sequence")
 
 
 def dict_to_tuple(d):
--- tests.py
+++ tests.py
@@ -4,52 +4,70 @@ import phpserialize
 
 
 class PhpSerializeTestCase(unittest.TestCase):
-
     def test_dumps_int(self):
-        self.assertEqual(phpserialize.dumps(5), b'i:5;')
+        self.assertEqual(phpserialize.dumps(5), b"i:5;")
 
     def test_dumps_float(self):
-        self.assertEqual(phpserialize.dumps(5.6), b'd:5.6;')
+        self.assertEqual(phpserialize.dumps(5.6), b"d:5.6;")
 
     def test_dumps_str(self):
-        self.assertEqual(phpserialize.dumps('Hello world'),
-                         b's:11:"Hello world";')
+        self.assertEqual(phpserialize.dumps("Hello world"), b's:11:"Hello world";')
 
     def test_dumps_unicode(self):
-        self.assertEqual(phpserialize.dumps('Björk Guðmundsdóttir'),
-                         b's:23:"Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir";')
+        self.assertEqual(
+            phpserialize.dumps("Björk Guðmundsdóttir"),
+            b's:23:"Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir";',
+        )
 
     def test_dumps_binary(self):
-        self.assertEqual(phpserialize.dumps(b'\001\002\003'),
-                         b's:3:"\x01\x02\x03";')
+        self.assertEqual(phpserialize.dumps(b"\001\002\003"), b's:3:"\x01\x02\x03";')
 
     def test_dumps_list(self):
-        self.assertEqual(phpserialize.dumps([7, 8, 9]),
-                         b'a:3:{i:0;i:7;i:1;i:8;i:2;i:9;}')
+        self.assertEqual(
+            phpserialize.dumps([7, 8, 9]), b"a:3:{i:0;i:7;i:1;i:8;i:2;i:9;}"
+        )
 
     def test_dumps_tuple(self):
-        self.assertEqual(phpserialize.dumps((7, 8, 9)),
-                         b'a:3:{i:0;i:7;i:1;i:8;i:2;i:9;}')
+        self.assertEqual(
+            phpserialize.dumps((7, 8, 9)), b"a:3:{i:0;i:7;i:1;i:8;i:2;i:9;}"
+        )
 
     def test_dumps_dict(self):
-        self.assertEqual(phpserialize.dumps({'a': 1, 'b': 2, 'c': 3}),
-                         b'a:3:{s:1:"a";i:1;s:1:"c";i:3;s:1:"b";i:2;}')
+        self.assertEqual(
+            phpserialize.dumps({"a": 1, "b": 2, "c": 3}),
+            b'a:3:{s:1:"a";i:1;s:1:"b";i:2;s:1:"c";i:3;}',
+        )
 
     def test_loads_dict(self):
-        self.assertEqual(phpserialize.loads(b'a:3:{s:1:"a";i:1;s:1:"c";i:3;s:1:"b";i:2;}',
-                         decode_strings=True), {'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(
+            phpserialize.loads(
+                b'a:3:{s:1:"a";i:1;s:1:"c";i:3;s:1:"b";i:2;}', decode_strings=True
+            ),
+            {"a": 1, "b": 2, "c": 3},
+        )
 
     def test_loads_unicode(self):
-        self.assertEqual(phpserialize.loads(b's:23:"Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir";',
-                         decode_strings=True), b'Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir'.decode('utf-8'))
+        self.assertEqual(
+            phpserialize.loads(
+                b's:23:"Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir";',
+                decode_strings=True,
+            ),
+            b"Bj\xc3\xb6rk Gu\xc3\xb0mundsd\xc3\xb3ttir".decode("utf-8"),
+        )
 
     def test_loads_binary(self):
-        self.assertEqual(phpserialize.loads(b's:3:"\001\002\003";', decode_strings=False),
-                         b'\001\002\003')
+        self.assertEqual(
+            phpserialize.loads(b's:3:"\001\002\003";', decode_strings=False),
+            b"\001\002\003",
+        )
 
     def test_dumps_and_loads_dict(self):
-        self.assertEqual(phpserialize.loads(phpserialize.dumps({'a': 1, 'b': 2, 'c': 3}),
-                         decode_strings=True), {'a': 1, 'b': 2, 'c': 3})
+        self.assertEqual(
+            phpserialize.loads(
+                phpserialize.dumps({"a": 1, "b": 2, "c": 3}), decode_strings=True
+            ),
+            {"a": 1, "b": 2, "c": 3},
+        )
 
     def test_list_roundtrips(self):
         x = phpserialize.loads(phpserialize.dumps(list(range(2))))
@@ -77,28 +95,42 @@ class PhpSerializeTestCase(unittest.TestCase):
                 self.username = username
 
         def load_object_hook(name, d):
-            return {'WP_User': User}[name](**d)
+            return {"WP_User": User}[name](**d)
 
         def dump_object_hook(obj):
             if isinstance(obj, User):
-                return phpserialize.phpobject('WP_User', {'username': obj.username})
-            raise LookupError('unknown object')
+                return phpserialize.phpobject("WP_User", {"username": obj.username})
+            raise LookupError("unknown object")
 
-        user = User('test')
+        user = User("test")
         x = phpserialize.dumps(user, object_hook=dump_object_hook)
-        y = phpserialize.loads(x, object_hook=load_object_hook,
-                               decode_strings=True)
-        self.assert_(b'WP_User' in x)
+        y = phpserialize.loads(x, object_hook=load_object_hook, decode_strings=True)
+        self.assertTrue(b"WP_User" in x)
         self.assertEqual(type(y), type(user))
         self.assertEqual(y.username, user.username)
 
     def test_basic_object_hook(self):
         data = b'O:7:"WP_User":1:{s:8:"username";s:5:"admin";}'
-        user = phpserialize.loads(data, object_hook=phpserialize.phpobject,
-                                  decode_strings=True)
-        self.assertEqual(user.username, 'admin')
-        self.assertEqual(user.__name__, 'WP_User')
+        user = phpserialize.loads(
+            data, object_hook=phpserialize.phpobject, decode_strings=True
+        )
+        self.assertEqual(user.username, "admin")
+        self.assertEqual(user.__name__, "WP_User")
+
+    def test_reference(self):
+        data = b'a:2:{i:0;s:3:"foo";i:1;R:2;}'
+        result = phpserialize.loads(data)
+
+        self.assertEqual(result, {0: b"foo", 1: b"foo"})
+
+    def test_reference_nested(self):
+        data = b'a:2:{s:2:"na";a:2:{s:2:"te";s:2:"st";s:2:"xa";s:2:"xi";}s:9:"refstring";R:4;}'
+        result = phpserialize.loads(data)
+
+        self.assertEqual(
+            result, {b"na": {b"te": b"st", b"xa": b"xi"}, b"refstring": b"xi"}
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
